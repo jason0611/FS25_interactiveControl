be72e241d08a662a6f15537c25ddeaa117876e28
Glowins Modschmiede <martin@eller-essen.de>
2025-06-11 17:24:01 +0200

    Update InteractiveControl.lua

--- src/vehicles/specializations/InteractiveControl.lua
+++ src/vehicles/specializations/InteractiveControl.lua
@@ -283,7 +283,7 @@ function InteractiveControl:onUpdateTick(dt, isActiveForInput, isActiveForInputI
     local isOutdoor = self:isOutdoorActive()
 
     -- shows a + sign when ic is active and noHud mod enabled, also outside near by the vehicle trigger (SbSh, GlowinsModschmiede)
-    if (g_currentMission.hud.controlledVehicle == nil or self == g_currentMission.hud.controlledVehicle) and ( spec.isPlayerInRange or self:isIndoorActive() or self:isOutdoorActive() and not g_currentMission.hud:getIsVisible() ) then
+    if (g_currentMission.hud.controlledVehicle == nil or self == g_currentMission.hud.controlledVehicle) and not g_currentMission.hud:getIsVisible() and self:isIndoorActive() then
         if spec.state == true then
             renderText(0.5, 0.5, 0.018, "+")
         end
